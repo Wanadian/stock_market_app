5152b3aa2a2646e302c1f1572f65a4d6788b93ed
Aves <carlier.amandine98@gmail.com>
2023-09-14 09:48:45 +0200

    feat(project): Inherited Widget and Shared preferences initalised

---
+++ lib/context/inheritedWallet.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:stock_market_app/screens/wallet.dart';
+
+class InheritedWallet extends InheritedWidget {
+  final Wallet wallet;
+  final Widget child;
+  // Exemple de méthode appelant Wallet
+  final VoidCallback saveWalletAmount;
+
+  InheritedWallet({required this.wallet, required this.child, required this.saveWalletAmount})
+      : super(child: child);
+
+  static InheritedWallet of(BuildContext context) {
+    final InheritedWallet? result = context.dependOnInheritedWidgetOfExactType<InheritedWallet>();
+    assert(result != null, 'No InheritedCartItems found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(InheritedWallet oldWidget) {
+    return true;
+  }
+}
