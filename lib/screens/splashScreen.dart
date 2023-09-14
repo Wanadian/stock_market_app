bb421968f5ff182f2ec5a79f086308c0287e165f
Aves <carlier.amandine98@gmail.com>
2023-09-14 11:25:21 +0200

    feat(splash-screen): splash screen added

---
+++ lib/screens/splashScreen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:stock_market_app/screens/wallet.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _Splash();
+}
+
+class _Splash extends State<SplashPage> {
+  _Splash() {
+    pageSwapping();
+  }
+
+  void pageSwapping() async {
+    await Future.delayed(const Duration(seconds: 3));
+    Navigator.pushReplacement<void, void>(
+      context,
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) => const Wallet(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Socket',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height / 15),
+              child: Image.asset('assets/stocket-logo.png',
+                  width: MediaQuery.of(context).size.width / 3.5),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.grey.shade800,
+    );
+  }
+}
