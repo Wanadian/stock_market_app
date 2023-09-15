fdbc1df55527f7fe3da965da41a95d490f6db946
Wonkruu <williamdpe@gmail.com>
2023-09-15 17:00:35 +0200

    delete(share banner widget): comment

--- lib/widgets/shareBannerWidget.dart
+++ lib/widgets/shareBannerWidget.dart
@@ -39,7 +39,7 @@ class ShareBannerWidget extends StatelessWidget {
                   color: Colors.grey.withOpacity(0.5),
                   spreadRadius: 2,
                   blurRadius: 5,
-                  offset: Offset(0, 0), // changes position of shadow
+                  offset: Offset(0, 0),
                 ),
               ],
             ),
