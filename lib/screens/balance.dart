983bebcdf66d9e7e2a71c2910035546806c3cd95
Wonkruu <williamdpe@gmail.com>
2023-10-15 16:12:00 +0200

    fix(project): design

--- lib/screens/balance.dart
+++ lib/screens/balance.dart
@@ -2,7 +2,6 @@ import 'package:animated_digit/animated_digit.dart';
 import 'package:flutter/material.dart';
 import 'package:stock_market_app/screens/modifyBalance.dart';
 import 'package:stock_market_app/screens/stock-market/stockMarketAppBar.dart';
-import 'package:stock_market_app/widgets/buttonWidget.dart';
 
 class Balance extends StatefulWidget {
   const Balance({Key? key}) : super(key: key);
@@ -13,8 +12,9 @@ class Balance extends StatefulWidget {
 
 class _BalanceState extends State<Balance> {
   bool _isSafeOpen = false;
+
   //TODO : create a shared balance that is accessible throughout the app and that is saved when the app is closed
-  double _balance = 1000000000000.9;
+  double _balance = 999999;
   double _gapHeight = 0;
 
   void setIsSafeOpenToOpposite(bool isSafeOpen) {
@@ -35,68 +35,90 @@ class _BalanceState extends State<Balance> {
     });
   }
 
+  String _getMoneyImage(double amount) {
+    if (amount <= 1000) {
+      return 'assets/coins.png';
+    } else if (amount > 1000 && amount <= 10000) {
+      return 'assets/money.png';
+    } else {
+      return 'assets/money-bag.png';
+    }
+  }
+
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: Column(
-        children: [
-          if (_isSafeOpen) ...[
-            Container(height: screenHeight * 0.25),
-            AnimatedDigitWidget(
-              duration: Duration(seconds: 1),
-              value: _balance,
-              enableSeparator: true,
-              fractionDigits: 1,
-              suffix: ' \$',
-              textStyle: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 23),
+        body: Column(
+          children: [
+            if (_isSafeOpen) ...[
+              Container(height: screenHeight * 0.3),
+              AnimatedDigitWidget(
+                duration: Duration(seconds: 1),
+                value: _balance,
+                enableSeparator: true,
+                fractionDigits: 1,
+                suffix: ' \$',
+                textStyle: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 23),
+              )
+            ] else ...[
+              Container(height: screenHeight * 0.3),
+              Text('Click to display your balance',
+                  style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold))
+            ],
+            AnimatedContainer(
+              height: _gapHeight,
+              duration: Duration(milliseconds: 500),
             ),
-            Container(height: screenHeight * 0.02),
-            ButtonWidget.iconButton(
-                icon: Icons.add,
+            IconButton(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              icon: _isSafeOpen
+                  ? Image.asset(_getMoneyImage(_balance),
+                      width: screenWidth * 0.5)
+                  : Image.asset('assets/wallet.png', width: screenWidth * 0.5),
+              iconSize: 300,
+              onPressed: () {
+                setGapHeight(screenHeight * 0.03);
+                setIsSafeOpenToOpposite(_isSafeOpen);
+              },
+            )
+          ],
+        ),
+        floatingActionButton: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              FloatingActionButton.extended(
+                heroTag: 'modify-balance-button',
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.white,
+                label: Text('Add funds'),
+                icon: Icon(Icons.add),
                 onPressed: () {
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => ModifyBalance()));
                 },
-                height: screenHeight * 0.04,
-                width: screenWidth * 0.15)
-          ] else ...[
-            Container(height: screenHeight * 0.3)
-          ],
-          AnimatedContainer(
-            height: _gapHeight,
-            duration: Duration(milliseconds: 500),
-          ),
-          IconButton(
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-            icon: _isSafeOpen
-                //TODO: find new images
-                ? Image.asset('assets/open_safe.png', width: screenWidth * 0.6)
-                : Image.asset('assets/closed_safe.png',
-                    width: screenWidth * 0.5),
-            iconSize: 300,
-            onPressed: () {
-              setGapHeight(screenHeight * 0.03);
-              setIsSafeOpenToOpposite(_isSafeOpen);
-            },
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        foregroundColor: Colors.black,
-        backgroundColor: Colors.white,
-        label: Text('Stock market'),
-        icon: Icon(Icons.attach_money),
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => StockMarketAppBar(balance: _balance)));
-        },
-      ),
-    );
+              ),
+              Container(width: 0, height: screenHeight * 0.02),
+              FloatingActionButton.extended(
+                heroTag: 'stock-market-button',
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.white,
+                label: Text('Stock market'),
+                icon: Icon(Icons.attach_money),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              StockMarketAppBar(balance: _balance)));
+                },
+              )
+            ]));
   }
 }
