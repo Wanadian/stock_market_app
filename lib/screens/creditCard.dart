bdd21fb42c91398d741206449e242c5d9835cf10
Wonkruu <williamdpe@gmail.com>
2023-10-19 23:26:35 +0200

    wip

--- lib/screens/creditCard.dart
+++ lib/screens/creditCard.dart
@@ -1,18 +1,173 @@
 import 'package:flutter/material.dart';
+import 'package:stock_market_app/entities/cardEntity.dart';
+import 'package:stock_market_app/screens/modifyBalance.dart';
+import 'package:stock_market_app/services/cardService.dart';
+
+import '../context/inheritedServices.dart';
+import '../services/walletService.dart';
+import '../widgets/form/fields/dateFieldWidget.dart';
+import '../widgets/form/fields/numberFieldWidget.dart';
+import '../widgets/form/fields/textFieldWidget.dart';
+import '../widgets/form/formWidget.dart';
 
 class CreditCard extends StatefulWidget {
-  const CreditCard({Key? key}) : super(key: key);
+  CreditCard({Key? key, required int amount})
+      : _amount = amount,
+        super(key: key);
+
+  int _amount;
 
   @override
   State<CreditCard> createState() => _CreditCardState();
 }
 
 class _CreditCardState extends State<CreditCard> {
+  String _cardLabel = '';
+  String _cardHolderName = '';
+  int _cardNumber = -1;
+  int _cardSafeCode = -1;
+  DateTime _cardExpirationDate = DateTime.now();
+
+  GlobalKey<FormState> _cardDetailsForm = GlobalKey<FormState>();
+
+  TextEditingController _cardLabelController = TextEditingController();
+  TextEditingController _cardHolderNameController = TextEditingController();
+  TextEditingController _cardNumberController = TextEditingController();
+  TextEditingController _cardSafeCodeController = TextEditingController();
+
+  _creditAccount(WalletService walletService) async {
+    await walletService.creditWalletBalance(0.0);
+  }
+
+  _saveCardIfNew(CardService cardService) async {
+    bool exists = false;
+    List<CardEntity>? cardList = await cardService.getAllCards();
+    for (CardEntity card in cardList!) {
+      if (card.label == _cardLabel) {
+        exists = true;
+      }
+    }
+    if (!exists) {
+      cardService.addCard(_cardLabel, _cardHolderName, _cardNumber,
+          _cardSafeCode, _cardExpirationDate);
+    }
+  }
 
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
-    return Container();
+    var inheritedServices = InheritedServices.of(context);
+
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+            child: Column(children: [
+          Container(height: screenHeight * 0.07),
+          Align(
+              alignment: Alignment.centerLeft,
+              child: IconButton(
+                  icon: Icon(Icons.arrow_back_ios_new_rounded),
+                  color: Colors.white,
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ModifyBalance()));
+                  })),
+          Text('New card',
+              style: TextStyle(color: Colors.white, fontSize: 20)),
+          Container(height: screenHeight * 0.05),
+          FormWidget(
+            key: _cardDetailsForm,
+            fields: [
+              TextFieldWidget(
+                controller: _cardLabelController,
+                validator: (value) {
+                  if (value == '' || value == null) {
+                    return "Please enter the card holder's name";
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  setState(() {
+                    if (value != null) {
+                      _cardLabel = value;
+                    }
+                  });
+                },
+                label: 'Name you want to give to this card',
+              ),
+              TextFieldWidget(
+                controller: _cardHolderNameController,
+                validator: (value) {
+                  if (value == '' || value == null) {
+                    return "Please enter the card holder's name";
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  setState(() {
+                    if (value != null) {
+                      _cardHolderName = value;
+                    }
+                  });
+                },
+                label: 'Name on the card',
+              ),
+              NumberFieldWidget(
+                controller: _cardNumberController,
+                validator: (value) {
+                  if (value == '' || value == null || int.parse(value) < 0) {
+                    return 'Please enter a valid card number';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  setState(() {
+                    if (value != null) {
+                      _cardNumber = int.parse(value);
+                    }
+                  });
+                },
+                label: 'Card number',
+              ),
+              NumberFieldWidget(
+                controller: _cardSafeCodeController,
+                validator: (value) {
+                  if (value == '' || value == null || int.parse(value) < 0) {
+                    return 'Please enter a valid card safe code';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  setState(() {
+                    if (value != null) {
+                      _cardSafeCode = int.parse(value);
+                    }
+                  });
+                },
+                label: 'Card safe code',
+              ),
+              DateFieldWidget(
+                onChange: (DateTime date) {
+                  _cardExpirationDate = date;
+                },
+                label: 'Expiration date',
+              ),
+              Text('Your cards',
+                  style: TextStyle(color: Colors.white, fontSize: 20)),
+              //TODO insert selector for existing card
+            ],
+            onPressed: () {
+              _creditAccount(inheritedServices.walletService);
+              _saveCardIfNew(inheritedServices.cardService);
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ModifyBalance()));
+            },
+          ),
+        ])));
   }
 }
--- lib/screens/modifyBalance.dart
+++ lib/screens/modifyBalance.dart
@@ -1,12 +1,11 @@
 import 'package:animated_digit/animated_digit.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:stock_market_app/screens/creditCard.dart';
 
 import '../context/inheritedServices.dart';
 import '../services/walletService.dart';
-import '../widgets/form/fields/dateFieldWidget.dart';
 import '../widgets/form/fields/numberFieldWidget.dart';
-import '../widgets/form/fields/textFieldWidget.dart';
 import '../widgets/form/formWidget.dart';
 import 'balance.dart';
 
@@ -37,14 +36,11 @@ class ModifyBalance extends StatefulWidget {
 }
 
 class _ModifyBalanceState extends State<ModifyBalance> {
-  int _valueToPay = -1;
-  String _cardHolderName = '';
-  int _cardNumber = -1;
-  int _cardSafeCode = -1;
-  DateTime _cardExpirationDate = DateTime.now();
+  int _amount = -1;
 
-  GlobalKey<FormState> _valueToPayFrom = GlobalKey<FormState>();
-  GlobalKey<FormState> _cardDetailsForm = GlobalKey<FormState>();
+  GlobalKey<FormState> _amountFrom = GlobalKey<FormState>();
+
+  TextEditingController _amountController = TextEditingController();
 
   Future<String?> _getBalanceRequest(WalletService walletService) async {
     return await walletService.getWalletBalanceAsString();
@@ -53,11 +49,6 @@ class _ModifyBalanceState extends State<ModifyBalance> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-
-    TextEditingController _valueToPayController = TextEditingController();
-    TextEditingController _cardHolderNameController = TextEditingController();
-    TextEditingController _cardNumberController = TextEditingController();
-    TextEditingController _cardSafeCodeController = TextEditingController();
 
     var inheritedServices = InheritedServices.of(context);
     Future<String?> _balance =
@@ -68,216 +59,91 @@ class _ModifyBalanceState extends State<ModifyBalance> {
         builder: ((context, balance) {
           return Scaffold(
               resizeToAvoidBottomInset: false,
-              body: Container(
-                child: Column(
-                  children: [
-                    Container(height: screenHeight * 0.07),
-                    Align(
-                        alignment: Alignment.centerLeft,
-                        child: IconButton(
-                            icon: Icon(Icons.arrow_back_ios_new_rounded),
-                            color: Colors.white,
-                            splashColor: Colors.transparent,
-                            highlightColor: Colors.transparent,
-                            onPressed: () {
+              body: Column(children: [
+                Container(height: screenHeight * 0.07),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: IconButton(
+                        icon: Icon(Icons.arrow_back_ios_new_rounded),
+                        color: Colors.white,
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Balance()));
+                        })),
+                Container(height: screenHeight * 0.1),
+                Text('Current balance',
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+                Container(height: screenHeight * 0.05),
+                if (balance.hasData) ...[
+                  AnimatedDigitWidget(
+                    duration: Duration(seconds: 1),
+                    value: double.parse(balance.data!),
+                    enableSeparator: true,
+                    fractionDigits: 1,
+                    suffix: ' \$',
+                    textStyle: TextStyle(color: Colors.white, fontSize: 20),
+                  )
+                ] else if (balance.hasError) ...[
+                  Text("Something went wrong, we can't load your balance",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.red, fontSize: 15))
+                ] else ...[
+                  const CircularProgressIndicator()
+                ],
+                Container(height: screenHeight * 0.08),
+                Text('How much do you want to add ?',
+                    style: TextStyle(color: Colors.white, fontSize: 20)),
+                Container(height: screenHeight * 0.05),
+                Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Container(width: screenWidth * 0.1),
+                  Container(
+                      constraints: BoxConstraints(
+                          minWidth: 0, maxWidth: screenWidth * 0.8),
+                      child: FormWidget(
+                          key: _amountFrom,
+                          buttonLabel: 'Pay',
+                          fields: [
+                            NumberFieldWidget(
+                              controller: _amountController,
+                              validator: (value) {
+                                if (value == '' ||
+                                    value == null ||
+                                    int.parse(value) <= 0) {
+                                  return 'Please enter a value greater than 0';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                setState(() {
+                                  if (value != null) {
+                                    _amount = int.parse(value);
+                                  }
+                                });
+                              },
+                              label: 'Value you want to credit',
+                            ),
+                          ],
+                          onPressed: () {
+                            if (_amountFrom.currentState!.validate()) {
+                              _amountFrom.currentState?.save();
                               Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => Balance()));
-                            })),
-                    Container(height: screenHeight * 0.1),
-                    Text('Current balance',
-                        style: TextStyle(color: Colors.white, fontSize: 20)),
-                    Container(height: screenHeight * 0.05),
-                    if (balance.hasData) ...[
-                      AnimatedDigitWidget(
-                        duration: Duration(seconds: 1),
-                        value: double.parse(balance.data!),
-                        enableSeparator: true,
-                        fractionDigits: 1,
-                        suffix: ' \$',
-                        textStyle: TextStyle(color: Colors.white, fontSize: 20),
-                      )
-                    ] else if (balance.hasError) ...[
-                      Text("Something went wrong, we can't load your balance",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.red, fontSize: 15))
-                    ] else ...[
-                      const CircularProgressIndicator()
-                    ],
-                    Container(height: screenHeight * 0.08),
-                    Text('How much do you want to add ?',
-                        style: TextStyle(color: Colors.white, fontSize: 20)),
-                    Container(height: screenHeight * 0.05),
-                    Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(width: screenWidth * 0.1),
-                          Container(
-                            constraints: BoxConstraints(
-                                minWidth: 0, maxWidth: screenWidth * 0.8),
-                            child: FormWidget(
-                              key: _valueToPayFrom,
-                              buttonLabel: 'Pay',
-                              fields: [
-                                NumberFieldWidget(
-                                  controller: _valueToPayController,
-                                  validator: (value) {
-                                    if (value == '' ||
-                                        value == null ||
-                                        int.parse(value) <= 0) {
-                                      return 'Please enter a value greater than 0';
-                                    }
-                                    return null;
-                                  },
-                                  onSaved: (value) {
-                                    setState(() {
-                                      if (value != null) {
-                                        _valueToPay = int.parse(value);
-                                      }
-                                    });
-                                  },
-                                  label: 'Value you want to credit',
-                                ),
-                              ],
-                              onPressed: () {
-                                if (_valueToPayFrom.currentState!.validate()) {
-                                  _valueToPayFrom.currentState?.save();
-                                  showDialog(
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        return AlertDialog(
-                                            shadowColor: Colors.grey.shade800,
-                                            surfaceTintColor: Colors.black,
-                                            backgroundColor: Colors.transparent,
-                                            elevation: 10.0,
-                                            scrollable: true,
-                                            title: Text(
-                                                'Enter your card details',
-                                                style: TextStyle(
-                                                    color: Colors.white),
-                                                textAlign: TextAlign.center),
-                                            content: Padding(
-                                                padding:
-                                                    const EdgeInsets.all(0.8),
-                                                child: Column(children: [
-                                                  Container(
-                                                      height:
-                                                          screenHeight * 0.05),
-                                                  FormWidget(
-                                                    key: _cardDetailsForm,
-                                                    fields: [
-                                                      TextFieldWidget(
-                                                        controller:
-                                                            _cardHolderNameController,
-                                                        validator: (value) {
-                                                          if (value == '' ||
-                                                              value == null) {
-                                                            return "Please enter the card holder's name";
-                                                          }
-                                                          return null;
-                                                        },
-                                                        onSaved: (value) {
-                                                          setState(() {
-                                                            if (value != null) {
-                                                              _cardHolderName =
-                                                                  value;
-                                                            }
-                                                          });
-                                                        },
-                                                        label:
-                                                            'Card holder name',
-                                                      ),
-                                                      NumberFieldWidget(
-                                                        controller:
-                                                            _cardNumberController,
-                                                        validator: (value) {
-                                                          if (value == '' ||
-                                                              value == null ||
-                                                              int.parse(value) <
-                                                                  0) {
-                                                            return 'Please enter a valid card number';
-                                                          }
-                                                          return null;
-                                                        },
-                                                        onSaved: (value) {
-                                                          setState(() {
-                                                            if (value != null) {
-                                                              _cardNumber =
-                                                                  int.parse(
-                                                                      value);
-                                                            }
-                                                          });
-                                                        },
-                                                        label: 'Card number',
-                                                      ),
-                                                      NumberFieldWidget(
-                                                        controller:
-                                                            _cardSafeCodeController,
-                                                        validator: (value) {
-                                                          if (value == '' ||
-                                                              value == null ||
-                                                              int.parse(value) <
-                                                                  0) {
-                                                            return 'Please enter a valid card safe code';
-                                                          }
-                                                          return null;
-                                                        },
-                                                        onSaved: (value) {
-                                                          setState(() {
-                                                            if (value != null) {
-                                                              _cardSafeCode =
-                                                                  int.parse(
-                                                                      value);
-                                                            }
-                                                          });
-                                                        },
-                                                        label: 'Card safe code',
-                                                      ),
-                                                      DateFieldWidget(
-                                                        onChange:
-                                                            (DateTime date) {
-                                                          _cardExpirationDate =
-                                                              date;
-                                                        },
-                                                        label:
-                                                            'Expiration date',
-                                                      )
-                                                    ],
-                                                    onPressed: () {
-                                                      print(PaymentDetails(
-                                                              valuePaid:
-                                                                  _valueToPay,
-                                                              cardHolderName:
-                                                                  _cardHolderName,
-                                                              cardNumber:
-                                                                  _cardNumber,
-                                                              cardSafeCode:
-                                                                  _cardSafeCode,
-                                                              cardExpirationDate:
-                                                                  DateFormat(
-                                                                          'yyyy-MM-dd')
-                                                                      .format(
-                                                                          _cardExpirationDate))
-                                                          .returnDetailsInString());
-                                                    },
-                                                  ),
-                                                ])));
-                                      });
-                                }
-                              },
-                            ),
-                          ),
-                          Container(width: screenWidth * 0.02),
-                          Container(
-                            margin: const EdgeInsets.only(top: 17),
-                              child: Text('\$',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20)))
-                        ]),
-                  ],
-                ),
-              ));
+                                      builder: (context) =>
+                                          CreditCard(amount: _amount)));
+                            }
+                          })),
+                  Container(width: screenWidth * 0.02),
+                  Container(
+                      margin: const EdgeInsets.only(top: 17),
+                      child: Text('\$',
+                          style: TextStyle(color: Colors.white, fontSize: 20)))
+                ]),
+              ]));
         }));
   }
 }
